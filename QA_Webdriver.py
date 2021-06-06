3bb4f444836ca611acc53c143c80d8dd6a4f720b
Anton Borovyk <54375146+TonyBorovyk@users.noreply.github.com>
2021-06-06 23:29:33 +0300

    Add files via upload

---
+++ QA_Webdriver.py
@@ -0,0 +1,21 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+browser = webdriver.Chrome()
+browser.get("https://prom.ua")
+
+Search_header = "холодильник"
+expected_text = "«холодильник»\nв Украине"
+time.sleep(4)
+browser.find_element_by_css_selector('[name="search_term"]').send_keys(Search_header)
+time.sleep(15)
+browser.find_element_by_css_selector('[name="search_term"]').send_keys(Keys.RETURN)
+time.sleep(15)
+current_text = browser.find_element_by_css_selector('h1.ek-text').text
+
+
+assert current_text == expected_text
+time.sleep(4)
+
